8f4e8517075f01995b0b9bfca23ef4e186a4c485
lipeng <me@raybon.vip>
2022-09-29 09:19:08 +0800

    更新方法注解

--- src/Robot.php
+++ src/Robot.php
@@ -2,9 +2,15 @@
 
 namespace RaybonLee\LaravelRebot;
 
+use Closure;
 use Illuminate\Support\Facades\Facade;
 use Mitoop\Robot\Robot as IRobot;
-
+/**
+ * @method $this group($group)
+ * @method sendTextMsg($title, $content, $at = [])
+ * @method sendMarkdownMsg($content, $at = [])
+ * @method $this extend($name, Closure $closure)
+ */
 class Robot extends Facade
 {
     protected static function getFacadeAccessor()
