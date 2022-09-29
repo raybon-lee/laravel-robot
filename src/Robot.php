5b278ab470838efbe16e085aa5f5bd9afc328507
lipeng <me@raybon.vip>
2022-09-29 09:52:00 +0800

    增加返回值

--- src/Robot.php
+++ src/Robot.php
@@ -6,10 +6,10 @@ use Closure;
 use Illuminate\Support\Facades\Facade;
 use Mitoop\Robot\Robot as IRobot;
 /**
- * @method static group($group)
+ * @method static IRobot group($group)
  * @method static sendTextMsg($title, $content, $at = [])
  * @method static sendMarkdownMsg($content, $at = [])
- * @method static extend($name, Closure $closure)
+ * @method static IRobot extend($name, Closure $closure)
  */
 class Robot extends Facade
 {
